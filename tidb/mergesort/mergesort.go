bdfbb12c51208a035b07494b1b37b63f388499e6
小梧 <jason.wyc@alibaba-inc.com>
2020-01-17 15:14:15 +0800

    bottom up merge sort finish

--- tidb/mergesort/mergesort.go
+++ tidb/mergesort/mergesort.go
@@ -10,19 +10,37 @@ func MergeSort(src []int64) {
 
 	temp := make([]int64, len(src))
 
-	mergeSort(src, 0, len(src)-1, temp)
+	// mergeSort(src, 0, len(src)-1, temp)
+	mergeSortBottomUp(src, temp)
 }
 
-// func mergeSortBottomUp(src []int64) {
+func minInt(a, b int) int {
+	if a < b {
+		return a
+	} else {
+		return b
+	}
+}
 
-// 	width := 0
+func mergeSortBottomUp(src, temp []int64) {
 
-// 	for i := 1; i < len(src); i += 2 * width {
-// 		left, middle, right = i, i+width, i+2*width
+	width := 1
 
-// 	}
+	for width = 1; width < len(src); width *= 2 {
 
-// }
+		for i := 0; i < len(src); i += 2 * width {
+			left, middle, right := i, i+width-1, i+2*width-1
+
+			if right >= len(src) {
+				right = minInt(right, len(src)-1)
+				middle = minInt(i+width-1, right)
+			}
+
+			mergeArray(src, temp, left, middle, right)
+		}
+	}
+
+}
 
 func mergeSort(src []int64, start, end int, temp []int64) {
 
@@ -35,6 +53,10 @@ func mergeSort(src []int64, start, end int, temp []int64) {
 	mergeSort(src, start, mid, temp)
 	mergeSort(src, mid+1, end, temp)
 
+	mergeArray(src, temp, start, mid, end)
+}
+
+func mergeArray(src, temp []int64, start, mid, end int) {
 	p1, p2, p3 := start, mid+1, start
 
 	for p1 <= mid && p2 <= end {
